a69150038c45b8921b825136e3bfba69bf16920c
EC2 Default User <ec2-user@ip-10-21-96-87.ec2.internal>
2024-05-30 06:00:52 +0000

    feat: docker

--- api/src/app.js
+++ api/src/app.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
   req.dbConnection = connection
   next()
 })
-app.use('/api', routes)
+app.use('/', routes)
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
--- api/src/config/config.js
+++ api/src/config/config.js
@@ -1,8 +1,10 @@
+import dotenv from 'dotenv'
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-export const __dirname = dirname(fileURLToPath(import.meta.url));
+dotenv.config()
 
+export const __dirname = dirname(fileURLToPath(import.meta.url));
 export default {
   PORT: process.env.PORT,
   DB_HOST: process.env.DB_HOST,
