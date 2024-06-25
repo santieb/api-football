2aad981072f5c75286ff57a4277dd4bb45ed5f1b
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2024-06-24 23:12:51 -0300

    feat: advances

--- api/src/controllers.js
+++ api/src/controllers.js
@@ -1,4 +1,4 @@
-const country = "Argentina";
+const country = "Argentina"
 
 const RESULT = {
   WIN: "win",
@@ -6,51 +6,59 @@ const RESULT = {
   DRAW: "draw",
 };
 
+export const getFootballMatches = (matches, country) => {
+  const counters = {
+    [RESULT.WIN]: 0,
+    [RESULT.LOSE]: 0,
+    [RESULT.DRAW]: 0,
+  };
+
+  const matchesWithResult = matches.map((match) => {
+    const { home_team, home_score, away_team, away_score } = match;
+    let result
+
+    if (home_team === country && home_score > away_score) {
+      result = RESULT.WIN;
+      counters[RESULT.WIN] += 1
+    }
+
+    if (away_team === country && home_score < away_score) {
+      result = RESULT.LOSE;
+      counters[RESULT.LOSE] += 1
+    }
+
+    if (home_score === away_score) {
+      result = RESULT.DRAW;
+      counters[RESULT.DRAW] += 1
+    }
+
+    return {
+      ...match,
+      result,
+    };
+  });
+
+  const lastMatchesWithResult = matchesWithResult.slice(0, 14)
+
+  return { lastMatchesWithResult, counters };
+}
+
 const controllers = {
   getFootballMatches: (req, res) => {
-    const query = `select * from football_matches where away_team = "${country}" or home_team = "${country}" order by date desc`;
-    const connection = req.dbConnection;
+    const query = `select * from football_matches where away_team = "${country}" or home_team = "${country}" order by date desc`
+    const connection = req.dbConnection
 
     connection.query(query, (err, matches) => {
       if (err) {
-        console.error("Error executing query:", err);
-        res.status(500).send("Error executing query");
+        console.error("Error executing query:", err)
+        return res.status(500).send("Error executing query")
       }
 
-      const counters = {
-        WIN: 0,
-        LOSE: 0,
-        DRAW: 0,
-      };
-
-      const matchesWithResult = matches.map((match) => {
-        const { home_team, home_score, away_team, away_score } = match;
-        let result;
-
-        if (home_team === country && home_score > away_score) {
-          result = RESULT.WIN;
-          counters.WIN += 1;
-        }
-
-        if (away_team === country && home_score < away_score) {
-          result = RESULT.LOSE;
-          counters.LOSE += 1;
-        }
-
-        if (home_score === away_score) {
-          result = RESULT.DRAW;
-          counters.DRAW += 1;
-        }
-
-        match.result = result;
-        return match;
-      });
-
-      const lastMatchesWithResult = matches.slice(0, 14);
-
-      res.render("Home", { matches: lastMatchesWithResult, counters });
+      const { lastMatchesWithResult, counters } = getFootballMatches(matches, country)
+      console.log(lastMatchesWithResult, counters)
+      res.render("Home", { matches: lastMatchesWithResult, counters })
     });
   },
 };
 
-export default controllers;
+export default controllers
