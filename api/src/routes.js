d0a6c299066414cd5c99d381c7249dafa8be6f6a
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2024-06-27 00:41:47 -0300

    remove unused files

--- api/src/routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import controllers from './controllers.js'
-const router = Router()
-
-router
-  .get('/', controllers.getFootballMatches)
-
-export default router
